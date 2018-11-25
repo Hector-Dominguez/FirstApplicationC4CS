3038995da138296f68d4a0d6b929b3500e9a610c
Hector Dominguez <hdomingu@umich.edu>
2018-11-25 17:19:13 -0500

    new changes for the IOS app

--- FirstApplication/ViewController.h
+++ FirstApplication/ViewController.h
@@ -5,7 +5,7 @@
 //  Created by Hector Dominguez on 11/25/18.
 //  Copyright © 2018 Hector Dominguez. All rights reserved.
 //
-
+//hell ya
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
--- FirstApplication/ViewController.m
+++ FirstApplication/ViewController.m
@@ -9,10 +9,32 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic, weak) IBOutlet UILabel *messageLabel;
+@property (nonatomic, weak) IBOutlet UITextField *inputField;
 
 @end
 
 @implementation ViewController
+
+-(IBAction)Enter {
+    NSString *yourName = self.inputField.text;
+    NSString *myName = @"Hector";
+    NSString *message;
+    
+    if ([yourName length] == 0) {
+        yourName = @"World";
+    }
+    
+    if ([yourName isEqualToString:myName]) {
+        message = [NSString stringWithFormat:@"Hello %@ We have the same name", yourName];
+    }else {
+        message = [NSString stringWithFormat:@"Hello %@", yourName  ];
+        
+    }
+    
+    self.messageLabel.text = message;
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
